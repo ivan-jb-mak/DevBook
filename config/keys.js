785d56423b25c7aa9532a3c260eb7182caf3293b
ivan-jb-mak <ivanjbmak@gmail.com>
2021-05-16 23:06:42 -0400

    Adding mongoDB connectivity with mongoose in server.js

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://ivanjbmak:Ii..02021128@codebook.zwobl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+};
--- server.js
+++ server.js
@@ -1,9 +1,33 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
+// Database configuration
+const uri = require("./config/keys").mongoURI;
+
+//creating express server on port 5000
 const app = express();
-
-app.get("/", (req, res) => res.send("Hello!"));
-
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+// middleware with cors
+app.use(cors());
+app.use(express.json()); //to parse json
+
+// parser settings
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+// mongoose connection
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully!");
+});
+
+app.get("/", (req, res) => res.send("Hello Dabi"));
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
