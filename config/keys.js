aaf9c817f798c50174f190148638d129d389983f
ivan-jb-mak <ivanjbmak@gmail.com>
2021-05-18 16:48:43 -0400

    adding jwt(jsonwebtoken) functionality to login post method for users

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
     "mongodb+srv://ivanjbmak:Ii..02021128@codebook.zwobl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  secretOrKey: "secret",
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,8 @@ const User = require("../../models/User");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
 
 // @route   GET api/users/test
 // @desc    Tests users route
@@ -63,7 +65,21 @@ router.post("/login", (req, res) => {
     // Check Password
     bcrypt.compare(password, user.password).then((isMatch) => {
       if (isMatch) {
-        res.json({ msg: "Success" });
+        // User Matched
+        const payload = { id: user.id, name: user.name, avatar: user.avatar }; // Create JWT payload
+
+        // Sign token
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          { expiresIn: 3600 },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: "Bearer " + token,
+            });
+          }
+        );
       } else {
         return res.status(400).json({ password: "Password incorrect" });
       }
