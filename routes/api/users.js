c0460beaaa296e0cdfdec97d24dfb6d452d76c0b
ivan-jb-mak <ivanjbmak@gmail.com>
2021-05-19 22:45:08 -0400

    Adding validation handlers for login route

--- routes/api/users.js
+++ routes/api/users.js
@@ -9,6 +9,7 @@ const passport = require("passport");
 
 // Load input validation
 const validateRegisterInput = require("../../validation/register");
+const validateLoginInput = require("../../validation/login");
 
 // @route   GET api/users/test
 // @desc    Tests users route
@@ -64,6 +65,13 @@ router.post("/register", (req, res) => {
 // @desc    Login user / Returning JWT Token
 // @access  Public
 router.post("/login", (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  // Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   const email = req.body.email;
   const password = req.body.password;
 
@@ -71,7 +79,8 @@ router.post("/login", (req, res) => {
   User.findOne({ email }).then((user) => {
     // Check for user
     if (!user) {
-      return res.status(404).json({ email: "User not found" });
+      errors.email = "User not found";
+      return res.status(404).json(errors);
     }
 
     // Check Password
@@ -94,13 +103,14 @@ router.post("/login", (req, res) => {
         );
       } else {
         // error message, incorrect password
-        return res.status(400).json({ password: "Password incorrect" });
+        errors.password = "Password incorrect.";
+        return res.status(400).json(errors);
       }
     });
   });
 });
 
-// @route   POST api/users/current
+// @route   GET api/users/current
 // @desc    Return current user
 // @access  Private
 router.get(
---
+++ validation/login.js
@@ -0,0 +1,28 @@
+const Validator = require("validator");
+const isEmpty = require("./is-empty");
+
+module.exports = function validateLoginInput(data) {
+  let errors = {};
+
+  // Setting the field to an empty string if it's not empty
+  data.email = !isEmpty(data.email) ? data.email : "";
+  data.password = !isEmpty(data.password) ? data.password : "";
+
+  // Validation's and error messages
+  if (!Validator.isEmail(data.email)) {
+    errors.email = "Email is invalid.";
+  }
+
+  if (Validator.isEmpty(data.password)) {
+    errors.password = "Password field is required.";
+  }
+
+  if (Validator.isEmpty(data.email)) {
+    errors.email = "Email field is required.";
+  }
+
+  return {
+    errors, //equivilant to erros: errors
+    isValid: isEmpty(errors),
+  };
+};
