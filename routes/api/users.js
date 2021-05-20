e55fe138284a2879e783c0bd69d9c1401cc3d247
ivan-jb-mak <ivanjbmak@gmail.com>
2021-05-19 22:19:54 -0400

    adding validation handlers for the register route

--- routes/api/users.js
+++ routes/api/users.js
@@ -7,6 +7,9 @@ const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
 const passport = require("passport");
 
+// Load input validation
+const validateRegisterInput = require("../../validation/register");
+
 // @route   GET api/users/test
 // @desc    Tests users route
 // @access  Public
@@ -16,6 +19,13 @@ router.get("/test", (req, res) => res.json({ msg: "Users Works" }));
 // @desc    Register user
 // @access  Public
 router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  // Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({ email: req.body.email }).then((user) => {
     if (user) {
       return res.status(400).json({ email: "Email already exsists" });
