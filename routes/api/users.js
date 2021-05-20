ab5dfaa12cb30e230e41fd9365014bf9a6d70866
ivan-jb-mak <ivanjbmak@gmail.com>
2021-05-19 22:21:19 -0400

    editing register route to return errors.email instead of literal string

--- routes/api/users.js
+++ routes/api/users.js
@@ -28,7 +28,8 @@ router.post("/register", (req, res) => {
 
   User.findOne({ email: req.body.email }).then((user) => {
     if (user) {
-      return res.status(400).json({ email: "Email already exsists" });
+      errors.email = "Email already exsists";
+      return res.status(400).json(errors);
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: "200", // Size
