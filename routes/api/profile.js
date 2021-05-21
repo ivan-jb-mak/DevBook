492df99bc7aa224b2448025cb6fffe2524005637
ivan-jb-mak <ivanjbmak@gmail.com>
2021-05-20 22:45:31 -0400

    adding populate method in get profile route to show name and avatar.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -26,6 +26,7 @@ router.get(
     const errors = {};
 
     Profile.findOne({ user: req.user.id })
+      .populate("user", ["name", "avatar"])
       .then((profile) => {
         if (!profile) {
           errors.noprofile = "There is no profile for this user.";
