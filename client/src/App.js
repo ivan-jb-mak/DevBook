cafc69a67b57cee3f476e9588f6fe8c9fcb36e10
ivan-jb-mak <ivanjbmak@gmail.com>
2021-05-24 01:43:47 -0400

    Adding Header, Footer, and landing page react components

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
+import Footer from "./components/layout/Footer";
+import Landing from "./components/layout/Landing";
+import Navbar from "./components/layout/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <h1>My React App</h1>
+      <Navbar />
+      <Landing />
+      <Footer />
     </div>
   );
 }
