2cbac78eb6af02b40eb6ae49a8c6944b2131c9ae
ivan-jb-mak <ivanjbmak@gmail.com>
2021-05-24 17:34:54 -0400

    rewriting database connection with mongoDB with async and await

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://ivanjbmak:Ii..02021128@codebook.zwobl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-  secretOrKey: "secret",
-};
--- server.js
+++ server.js
@@ -1,55 +1,31 @@
-// Dependancies
 const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const passport = require("passport");
-
-// importing routes
-const users = require("./routes/api/users");
-const profile = require("./routes/api/profile");
-const posts = require("./routes/api/posts");
+const connectDB = require("./config/db");
+//const path = require("path");
 
 const app = express();
 
-// middleware with cors
-//app.use(cors());
-//app.use(express.json()); //to parse json
+// Connect Database
+connectDB();
+
+// Init Middleware
 app.use(express.json());
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
 
-const uri = require("./config/keys").mongoURI; // Database configuration URI
+// // Define Routes
+// app.use("/api/users", require("./routes/api/users"));
+// app.use("/api/auth", require("./routes/api/auth"));
+// app.use("/api/profile", require("./routes/api/profile"));
+// app.use("/api/posts", require("./routes/api/posts"));
 
-// parser settings
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-});
+// // Serve static assets in production
+// if (process.env.NODE_ENV === "production") {
+//   // Set static folder
+//   app.use(express.static("client/build"));
 
-// mongoose connection
-const connection = mongoose.connection;
-connection.once("open", () => {
-  console.log("MongoDB database connection established successfully!");
-});
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
 
-// Passport middleware
-app.use(passport.initialize());
+const PORT = process.env.PORT || 5000;
 
-// Passport Config
-require("./config/passport")(passport);
-
-// Use routes
-app.use("/api/users", users);
-app.use("/api/profile", profile);
-app.use("/api/posts", posts);
-
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
