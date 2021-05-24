75b1f480d0d7e1f00e0ec2e2af443a122c5aacb5
ivan-jb-mak <ivanjbmak@gmail.com>
2021-05-24 02:01:30 -0400

    adding react router with links to login and register component pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,24 @@
 import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
 import Navbar from "./components/layout/Navbar";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Landing />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <div className="container">
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </div>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
