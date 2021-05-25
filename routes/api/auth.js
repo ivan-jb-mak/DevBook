4aac7cdef6bbb2f80fed48440babcaf3a9d6f896
ivan-jb-mak <ivanjbmak@gmail.com>
2021-05-25 11:12:05 -0400

    Adding get route for getting user jwt for authentication

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,25 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
 // const bcrypt = require('bcryptjs');
-// const auth = require('../../middleware/auth');
+
 // const jwt = require('jsonwebtoken');
 // const config = require('config');
 // const { check, validationResult } = require('express-validator');
 
-// const User = require('../../models/User');
+const User = require("../../models/User");
 
-// // @route    GET api/auth
-// // @desc     Get user by token
-// // @access   Private
-// router.get('/', auth, async (req, res) => {
-//   try {
-//     const user = await User.findById(req.user.id).select('-password');
-//     res.json(user);
-//   } catch (err) {
-//     console.error(err.message);
-//     res.status(500).send('Server Error');
-//   }
-// });
+// @route    GET api/auth
+// @desc     Get user by token
+// @access   Private
+router.get("/", auth, async (req, res) => {
+  try {
+    // Find user by id, without the password
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user); // Sending user back
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 // // @route    POST api/auth
 // // @desc     Authenticate user & get token
