cfba06e079f8782e5b46a635e73f68140c053cd8
ivan-jb-mak <ivanjbmak@gmail.com>
2021-05-25 11:22:12 -0400

    Adding login authentication route to authenticate and get user token

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/auth");
-// const bcrypt = require('bcryptjs');
-
-// const jwt = require('jsonwebtoken');
-// const config = require('config');
-// const { check, validationResult } = require('express-validator');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { check, validationResult } = require("express-validator");
 
 const User = require("../../models/User");
 
@@ -23,58 +22,69 @@ router.get("/", auth, async (req, res) => {
   }
 });
 
-// // @route    POST api/auth
-// // @desc     Authenticate user & get token
-// // @access   Public
-// router.post(
-//   '/',
-//   check('email', 'Please include a valid email').isEmail(),
-//   check('password', 'Password is required').exists(),
-//   async (req, res) => {
-//     const errors = validationResult(req);
-//     if (!errors.isEmpty()) {
-//       return res.status(400).json({ errors: errors.array() });
-//     }
+// @route    POST api/auth
+// @desc     Authenticate user & get token
+// @access   Public
+router.post(
+  "/",
+  // Express-validation with error messages
+  [
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "Password is required").exists(),
+  ],
+  async (req, res) => {
+    // Handling response
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-//     const { email, password } = req.body;
+    // Detructuring req.body for user details
+    const { email, password } = req.body;
 
-//     try {
-//       let user = await User.findOne({ email });
+    try {
+      // See if user exists
+      let user = await User.findOne({ email });
 
-//       if (!user) {
-//         return res
-//           .status(400)
-//           .json({ errors: [{ msg: 'Invalid Credentials' }] });
-//       }
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid Credentials" }] });
+      }
 
-//       const isMatch = await bcrypt.compare(password, user.password);
+      // Comparing passwords with bcrypt
+      const isMatch = await bcrypt.compare(password, user.password);
 
-//       if (!isMatch) {
-//         return res
-//           .status(400)
-//           .json({ errors: [{ msg: 'Invalid Credentials' }] });
-//       }
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid Credentials" }] });
+      }
 
-//       const payload = {
-//         user: {
-//           id: user.id
-//         }
-//       };
+      // Return JsonWebToken
+      // Creating payload
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
 
-//       jwt.sign(
-//         payload,
-//         config.get('jwtSecret'),
-//         { expiresIn: '5 days' },
-//         (err, token) => {
-//           if (err) throw err;
-//           res.json({ token });
-//         }
-//       );
-//     } catch (err) {
-//       console.error(err.message);
-//       res.status(500).send('Server error');
-//     }
-//   }
-// );
+      // Creating token with payload
+      jwt.sign(
+        payload,
+        config.get("jwtToken"),
+        { expiresIn: "360000" },
+        (err, token) => {
+          if (err) throw err;
+          // Return token
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
 
 module.exports = router;
