b08971d18dc91ebeea413485826895a36d62e1b6
ivan-jb-mak <ivanjbmak@gmail.com>
2021-05-25 09:56:12 -0400

    adding register users route with express-validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,17 +1,38 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
 // const gravatar = require("gravatar");
 // const bcrypt = require("bcryptjs");
 // const jwt = require("jsonwebtoken");
 // const config = require("config");
-// const { check, validationResult } = require("express-validator");
 // const normalize = require("normalize-url");
 
 //const User = require("../../models/User");
 
-router.post("/", (req, res) => {
-  console.log(req.body);
-});
+// @route    POST api/users
+// @desc     Register user
+// @access   Public
+router.post(
+  "/",
+  // express-validation with error messages
+  [
+    check("name", "Name is required").notEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    // handling response
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      // sending back error messages in 400 response
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send("User route");
+  }
+);
 
 // // @route    POST api/users
 // // @desc     Register user
