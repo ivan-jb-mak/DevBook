4ceee3ea3c57ef26da699d8e2ee68d25f7c482df
ivan-jb-mak <ivanjbmak@gmail.com>
2021-05-26 12:10:50 -0400

    Adding routes to get github repos with github api

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -5,7 +5,8 @@ const axios = require("axios");
 const config = require("config");
 const { check, validationResult } = require("express-validator");
 // bring in normalize to give us a proper url, regardless of what user entered
-const normalize = require("normalize-url");
+const request = require("request");
+// const normalize = require("normalize-url");
 // const checkObjectId = require("../../middleware/checkObjectId");
 
 const Profile = require("../../models/Profile");
@@ -322,25 +323,36 @@ router.delete("/education/:edu_id", auth, async (req, res) => {
   }
 });
 
-// // @route    GET api/profile/github/:username
-// // @desc     Get user repos from Github
-// // @access   Public
-// router.get('/github/:username', async (req, res) => {
-//   try {
-//     const uri = encodeURI(
-//       `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`
-//     );
-//     const headers = {
-//       'user-agent': 'node.js',
-//       Authorization: `token ${config.get('githubToken')}`
-//     };
+// @route    GET api/profile/github/:username
+// @desc     Get user repos from Github
+// @access   Public
+router.get("/github/:username", (req, res) => {
+  try {
+    const options = {
+      uri: encodeURI(
+        `https://api.github.com/users/${
+          req.params.username
+        }/repos?per_page=5&sort=created:asc&client_id=${config.get(
+          "githubClientId"
+        )}&client_secret=${config.get("githubClientSecret")}`
+      ),
+      method: "GET",
+      headers: { "user-agent": "node.js" },
+    };
 
-//     const gitHubResponse = await axios.get(uri, { headers });
-//     return res.json(gitHubResponse.data);
-//   } catch (err) {
-//     console.error(err.message);
-//     return res.status(404).json({ msg: 'No Github profile found' });
-//   }
-// });
+    request(options, (error, response, body) => {
+      if (error) console.error(error);
+
+      if (response.statusCode !== 200) {
+        return res.status(404).json({ msg: "No Github profile found" });
+      }
+
+      res.json(JSON.parse(body));
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
