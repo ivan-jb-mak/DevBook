0445e8aba741fe031ab940c65d31dcd8684a725f
ivan-jb-mak <ivanjbmak@gmail.com>
2021-05-25 22:36:28 -0400

    Adding route to get a profile by their user ID

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -123,34 +123,33 @@ router.post(
 router.get("/", async (req, res) => {
   try {
     const profiles = await Profile.find().populate("user", ["name", "avatar"]);
-    res.json(profiles);
+    res.json(profiles); // Returning all profiles
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server Error");
   }
 });
 
-// // @route    GET api/profile/user/:user_id
-// // @desc     Get profile by user ID
-// // @access   Public
-// router.get(
-//   '/user/:user_id',
-//   checkObjectId('user_id'),
-//   async ({ params: { user_id } }, res) => {
-//     try {
-//       const profile = await Profile.findOne({
-//         user: user_id
-//       }).populate('user', ['name', 'avatar']);
+// @route    GET api/profile/user/:user_id
+// @desc     Get profile by user ID
+// @access   Public
+router.get("/user/:user_id", async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id,
+    }).populate("user", ["name", "avatar"]);
 
-//       if (!profile) return res.status(400).json({ msg: 'Profile not found' });
+    if (!profile) return res.status(400).json({ msg: "Profile not found" });
 
-//       return res.json(profile);
-//     } catch (err) {
-//       console.error(err.message);
-//       return res.status(500).json({ msg: 'Server error' });
-//     }
-//   }
-// );
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind == "ObjectId") {
+      return res.status(400).json({ msg: "Profile not found" });
+    }
+    res.status(500).send("Server Error");
+  }
+});
 
 // // @route    DELETE api/profile
 // // @desc     Delete profile, user & posts
