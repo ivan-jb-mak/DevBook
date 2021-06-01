e4d089d602d6e225168197a86a2a4b6bcd47a02b
ivan-jb-mak <ivanjbmak@gmail.com>
2021-06-01 02:03:04 -0400

    fixing

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -2,13 +2,15 @@ import { SET_ALERT, REMOVE_ALERT } from "./types";
 //import uuid from "uuid";
 import { v4 as uuid } from "uuid";
 
-export const setAlert = (msg, alertType) => (dispatch) => {
-  const id = uuid();
+export const setAlert =
+  (msg, alertType, timeout = 4000) =>
+  (dispatch) => {
+    const id = uuid();
 
-  dispatch({
-    type: SET_ALERT,
-    payload: { msg, alertType, id },
-  });
+    dispatch({
+      type: SET_ALERT,
+      payload: { msg, alertType, id },
+    });
 
-  setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), 3000);
-};
+    setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeout);
+  };
