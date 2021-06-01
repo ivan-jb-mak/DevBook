67d48a9aba3b58efeeb41e973be7255be7cb6ba9
ivan-jb-mak <ivanjbmak@gmail.com>
2021-05-31 23:55:40 -0400

    adding alert component for incorrect passwords

--- client/src/components/layout/Alert.js
+++ client/src/components/layout/Alert.js
@@ -2,10 +2,21 @@ import React from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
-const Alert = (props) => {
-  return <div></div>;
+const Alert = ({ alerts }) =>
+  alerts !== null &&
+  alerts.length > 0 &&
+  alerts.map((alert) => (
+    <div key={alert.id} className={`alert alert-${alert.alertType}`}>
+      {alert.msg}
+    </div>
+  ));
+
+Alert.propsTypes = {
+  alerts: PropTypes.array.isRequired,
 };
 
-Alert.propsTypes = {};
+const mapStateToProps = (state) => ({
+  alerts: state.alert,
+});
 
-export default connect()(Alert);
+export default connect(mapStateToProps)(Alert);
