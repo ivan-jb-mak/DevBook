6a53e656c8a1c0ac400747ed0955fea36e8218e7
ivan-jb-mak <ivanjbmak@gmail.com>
2021-06-02 15:00:47 -0400

    creating create profile action, and adding functionality to component fields

--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -1,8 +1,10 @@
 import React, { Fragment, useState } from "react";
+import { Link, withRouter } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import { createProfile } from "../../actions/profile";
 
-const CreateProfile = (props) => {
+const CreateProfile = ({ createProfile, history }) => {
   const [formData, setformData] = useState({
     company: "",
     website: "",
@@ -38,6 +40,11 @@ const CreateProfile = (props) => {
   const onChange = (e) =>
     setformData({ ...formData, [e.target.name]: e.target.value });
 
+  const onSubmit = (e) => {
+    e.preventDefault();
+    createProfile(formData, history);
+  };
+
   return (
     <Fragment>
       <h1 className="large text-primary">Create Your Profile</h1>
@@ -46,7 +53,7 @@ const CreateProfile = (props) => {
         profile stand out
       </p>
       <small>* = required field</small>
-      <form className="form">
+      <form className="form" onSubmit={(e) => onSubmit(e)}>
         <div className="form-group">
           <select name="status" value={status} onChange={(e) => onChange(e)}>
             <option value="0">* Select Professional Status</option>
@@ -68,7 +75,7 @@ const CreateProfile = (props) => {
             type="text"
             placeholder="Company"
             name="company"
-            value={status}
+            value={company}
             onChange={(e) => onChange(e)}
           />
           <small className="form-text">
@@ -125,11 +132,12 @@ const CreateProfile = (props) => {
           </small>
         </div>
         <div className="form-group">
-          <textarea placeholder="A short bio of yourself" name="bio">
-            {" "}
+          <textarea
+            placeholder="A short bio of yourself"
+            name="bio"
             value={bio}
             onChange={(e) => onChange(e)}
-          </textarea>
+          ></textarea>
           <small className="form-text">Tell us a little about yourself</small>
         </div>
 
@@ -212,6 +220,8 @@ const CreateProfile = (props) => {
   );
 };
 
-CreateProfile.propTypes = {};
+CreateProfile.propTypes = {
+  createProfile: PropTypes.func.isRequired,
+};
 
-export default connect()(CreateProfile);
+export default connect(null, { createProfile })(withRouter(CreateProfile));
