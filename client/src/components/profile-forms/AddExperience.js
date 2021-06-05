b2736189d1765119846dc2b9b928d03e660d83a4
ivanjbmak <ivanjbmak@gmail.com>
2021-06-05 00:08:56 -0400

    completeing the add-experience form and route to add experience

--- client/src/components/profile-forms/AddExperience.js
+++ client/src/components/profile-forms/AddExperience.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { addExperience } from "../../actions/profile";
 
-const AddExperience = (props) => {
+const AddExperience = ({ addExperience, history }) => {
   const [formData, setformData] = useState({
     company: "",
     title: "",
@@ -24,15 +24,21 @@ const AddExperience = (props) => {
 
   return (
     <Fragment>
-      <section class="container">
-        <h1 class="large text-primary">Add An Experience</h1>
-        <p class="lead">
-          <i class="fas fa-code-branch"></i> Add any developer/programming
+      <section className="container">
+        <h1 className="large text-primary">Add An Experience</h1>
+        <p className="lead">
+          <i className="fas fa-code-branch"></i> Add any developer/programming
           positions that you have had in the past
         </p>
         <small>* = required field</small>
-        <form class="form">
-          <div class="form-group">
+        <form
+          className="form"
+          onSubmit={(e) => {
+            e.preventDefault();
+            addExperience(formData, history);
+          }}
+        >
+          <div className="form-group">
             <input
               type="text"
               placeholder="* Job Title"
@@ -42,7 +48,7 @@ const AddExperience = (props) => {
               required
             />
           </div>
-          <div class="form-group">
+          <div className="form-group">
             <input
               type="text"
               placeholder="* Company"
@@ -52,7 +58,7 @@ const AddExperience = (props) => {
               required
             />
           </div>
-          <div class="form-group">
+          <div className="form-group">
             <input
               type="text"
               placeholder="Location"
@@ -61,7 +67,7 @@ const AddExperience = (props) => {
               onChange={(e) => onChange(e)}
             />
           </div>
-          <div class="form-group">
+          <div className="form-group">
             <h4>From Date</h4>
             <input
               type="date"
@@ -70,7 +76,7 @@ const AddExperience = (props) => {
               onChange={(e) => onChange(e)}
             />
           </div>
-          <div class="form-group">
+          <div className="form-group">
             <p>
               <input
                 type="checkbox"
@@ -85,7 +91,7 @@ const AddExperience = (props) => {
               Current Job
             </p>
           </div>
-          <div class="form-group">
+          <div className="form-group">
             <h4>To Date</h4>
             <input
               type="date"
@@ -95,7 +101,7 @@ const AddExperience = (props) => {
               disabled={toDateDisabled ? "disabled" : ""}
             />
           </div>
-          <div class="form-group">
+          <div className="form-group">
             <textarea
               name="description"
               cols="30"
@@ -105,10 +111,10 @@ const AddExperience = (props) => {
               onChange={(e) => onChange(e)}
             ></textarea>
           </div>
-          <input type="submit" class="btn btn-primary my-1" />
-          <a class="btn btn-light my-1" href="dashboard.html">
+          <input type="submit" className="btn btn-primary my-1" />
+          <Link className="btn btn-light my-1" to="/dashboard">
             Go Back
-          </a>
+          </Link>
         </form>
       </section>
     </Fragment>
