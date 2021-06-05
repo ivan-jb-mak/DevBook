f40554c2b8ecc1af350257485bca278dbf83a8bd
ivan-jb-mak <ivanjbmak@gmail.com>
2021-06-05 18:13:20 -0400

    adding posts

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -1,14 +1,28 @@
 import React, { Fragment, useEffect } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import PostItem from "./PostItem";
+//import PostForm from "./PostForm";
 import { getPosts } from "../../actions/post";
-import Spinner from "../layout/Spinner";
 
-const Posts = ({ getPosts, post: { posts, loading } }) => {
+const Posts = ({ getPosts, post: { posts } }) => {
   useEffect(() => {
     getPosts();
   }, [getPosts]);
-  return <div></div>;
+
+  return (
+    <Fragment>
+      <h1 className="large text-primary">Posts</h1>
+      <p className="lead">
+        <i className="fas fa-user" /> Welcome to the community
+      </p>
+      <div className="posts">
+        {posts.map((post) => (
+          <PostItem key={post._id} post={post} />
+        ))}
+      </div>
+    </Fragment>
+  );
 };
 
 Posts.propTypes = {
