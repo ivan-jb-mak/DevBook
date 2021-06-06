78e3c0e6cdb766ef48ce5fe096e04194a562d73f
ivanjbmak <ivanjbmak@gmail.com>
2021-06-05 23:55:00 -0400

    fixing postItem bug not dispalying posts

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from "react";
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import Moment from "react-moment";
 import { connect } from "react-redux";
 
@@ -8,40 +8,48 @@ const PostItem = ({
   auth,
   post: { _id, text, name, avatar, user, likes, comments, date },
 }) => (
-  <div class="post bg-white p-1 my-1">
+  <div className="post bg-white p-1 my-1">
     <div>
       <a href="profile.html">
-        <img class="round-img" src={avatar} alt="" />
-        <h4>John Doe</h4>
+        <img className="round-img" src={avatar} alt="" />
+        <h4>{name}</h4>
+        {console.log(name)}
       </a>
     </div>
     <div>
-      <p class="my-1">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint possimus
-        corporis sunt necessitatibus! Minus nesciunt soluta suscipit nobis. Amet
-        accusamus distinctio cupiditate blanditiis dolor? Illo perferendis
-        eveniet cum cupiditate aliquam?
+      <p className="my-1">{text}</p>
+      <p className="post-date">
+        Posted on <Moment format="YYYY/MM/DD">{date}</Moment>
       </p>
-      <p class="post-date">Posted on 04/16/2019</p>
-      <button type="button" class="btn btn-light">
-        <i class="fas fa-thumbs-up"></i>
-        <span>4</span>
+      <button type="button" className="btn btn-light">
+        <i className="fas fa-thumbs-up"></i>
+        <span>
+          {" "}
+          {likes.length > 0 && (
+            <span className="comment-count">{likes.length}</span>
+          )}
+        </span>
       </button>
-      <button type="button" class="btn btn-light">
-        <i class="fas fa-thumbs-down"></i>
+      <button type="button" className="btn btn-light">
+        <i className="fas fa-thumbs-down"></i>
       </button>
-      <a href="post.html" class="btn btn-primary">
-        Discussion <span class="comment-count">2</span>
-      </a>
-      <button type="button" class="btn btn-danger">
-        <i class="fas fa-times"></i>
-      </button>
+      <Link to={`/post/${_id}`} className="btn btn-primary">
+        Discussion{" "}
+        {comments.length > 0 && (
+          <span className="comment-count">{comments.length}</span>
+        )}
+      </Link>
+      {!auth.loading && user === auth.user._id && (
+        <button type="button" className="btn btn-danger">
+          <i className="fas fa-times"></i>
+        </button>
+      )}
     </div>
   </div>
 );
 
 PostItem.propTypes = {
-  post: PropTypes.object.isRequired,
+  post: PropTypes.array.isRequired,
   auth: PropTypes.object.isRequired,
 };
 
--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -7,7 +7,7 @@ const initialState = {
   error: {},
 };
 
-export default function post(state = initialState, action) {
+export default function (state = initialState, action) {
   const { type, payload } = action;
 
   switch (type) {
