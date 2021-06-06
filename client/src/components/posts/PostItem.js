b0094f11927ef71e6c70a8f583751d704d1bd371
ivanjbmak <ivanjbmak@gmail.com>
2021-06-05 23:07:56 -0400

    adding PostItem to Posts

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -8,33 +8,34 @@ const PostItem = ({
   auth,
   post: { _id, text, name, avatar, user, likes, comments, date },
 }) => (
-  <div className="post bg-white p-1 my-1">
+  <div class="post bg-white p-1 my-1">
     <div>
       <a href="profile.html">
-        <img className="round-img" src={avatar} alt="" />
-        <h4>{name}</h4>
+        <img class="round-img" src={avatar} alt="" />
+        <h4>John Doe</h4>
       </a>
     </div>
     <div>
-      <p className="my-1">{text}</p>
-      <p className="post-date">
-        Posted on <Moment format="YYY/MM/DD">{date}</Moment>
+      <p class="my-1">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint possimus
+        corporis sunt necessitatibus! Minus nesciunt soluta suscipit nobis. Amet
+        accusamus distinctio cupiditate blanditiis dolor? Illo perferendis
+        eveniet cum cupiditate aliquam?
       </p>
-      <button type="button" className="btn btn-light">
-        <i className="fas fa-thumbs-up"></i>
-        <span>{likes.length}</span>
+      <p class="post-date">Posted on 04/16/2019</p>
+      <button type="button" class="btn btn-light">
+        <i class="fas fa-thumbs-up"></i>
+        <span>4</span>
       </button>
-      <button type="button" className="btn btn-light">
-        <i className="fas fa-thumbs-down"></i>
+      <button type="button" class="btn btn-light">
+        <i class="fas fa-thumbs-down"></i>
       </button>
-      <a href="post.html" className="btn btn-primary">
-        Discussion <span className="comment-count">{comments.length}</span>
+      <a href="post.html" class="btn btn-primary">
+        Discussion <span class="comment-count">2</span>
       </a>
-      {!auth.loading && user === auth.user._id && (
-        <button type="button" className="btn btn-danger">
-          <i className="fas fa-times"></i>
-        </button>
-      )}
+      <button type="button" class="btn btn-danger">
+        <i class="fas fa-times"></i>
+      </button>
     </div>
   </div>
 );
--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -1,20 +1,23 @@
 import React, { Fragment, useEffect } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import Spinner from "../layout/Spinner";
 import PostItem from "./PostItem";
 //import PostForm from "./PostForm";
 import { getPosts } from "../../actions/post";
 
-const Posts = ({ getPosts, post: { posts } }) => {
+const Posts = ({ getPosts, post: { posts, loading } }) => {
   useEffect(() => {
     getPosts();
   }, [getPosts]);
 
-  return (
+  return loading ? (
+    <Spinner />
+  ) : (
     <Fragment>
       <h1 className="large text-primary">Posts</h1>
       <p className="lead">
-        <i className="fas fa-user" /> Welcome to the community
+        <i className="fas fa-user"></i> Welcome to the community
       </p>
       <div className="posts">
         {posts.map((post) => (
